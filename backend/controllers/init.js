ed3fbbd6ca8ed8156b9c44c05c5626e892d43a06
Official-Vinit <officialvinit1176@gmail.com>
2026-03-16 01:36:02 +0530

    init repo command

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 async function initRepo(){
-    console.log("Initializing a new Git repository...");
+    const repoPath = path.resolve(process.cwd(),".mygit");
+    const commitsPath = path.join(repoPath,"commits");
+
+    try{
+        await fs.mkdir(repoPath,{recursive: true});
+        await fs.mkdir(commitsPath, {recursive: true});
+        await fs.writeFile(
+            path.join(repoPath,"config.json"), 
+            JSON.stringify({bucket: "s3 bucket"})
+        );
+        console.log("Initialized empty MyGit repository in " + repoPath);
+    }catch(err){
+        console.error("Error initializing repository: ", err);
+    }
 }
 
 module.exports = {
